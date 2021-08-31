783d89921f3cdfd2fb4f791e323f23a58fb5ec42
hanmok <dmammmm@naver.com>
2021-08-31 15:29:20 +0900

    Initial Commit

---
+++ StatePractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StatePractice
+//
+//  Created by 이한목 on 2021/08/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StatePractice/StatePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatePracticeApp.swift
+//  StatePractice
+//
+//  Created by 이한목 on 2021/08/31.
+//
+
+import SwiftUI
+
+@main
+struct StatePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
